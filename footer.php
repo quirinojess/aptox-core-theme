579fab8e5a4f68e3fdde7c2d17ce3e08807c2f04
Jessica Quirino <qrnojess@gmail.com>
2026-06-14 02:21:01 -0300

    feat: adjust social midia section

--- footer.php
+++ footer.php
@@ -2,55 +2,58 @@
 /**
  * Global footer template.
  */
+
+$social_links = array(
+	array(
+		'url'   => 'https://www.instagram.com/aptox/',
+		'icon'  => 'ico-instagram.svg',
+		'label' => 'Instagram',
+	),
+	array(
+		'url'   => 'https://br.pinterest.com/aptoxblog/',
+		'icon'  => 'ico-pinterest.svg',
+		'label' => 'Pinterest',
+	),
+	array(
+		'url'   => 'https://www.youtube.com/@aptoxblog',
+		'icon'  => 'ico-youtube.svg',
+		'label' => 'YouTube',
+	),
+	array(
+		'url'   => 'https://www.tiktok.com/@aptoxblog',
+		'icon'  => 'ico-tiktok.svg',
+		'label' => 'TikTok',
+	),
+	array(
+		'url'   => 'https://www.facebook.com/aptox',
+		'icon'  => 'ico-facebook.svg',
+		'label' => 'Facebook',
+	),
+);
 ?>
 
 	</div>
 
-<section class="site-footer-midia">
-	<ul>
+<section
+	class="site-footer-midia"
+	aria-label="<?php esc_attr_e( 'Redes sociais', 'aptox' ); ?>"
+>
+	<ul class="site-footer-midia__list">
 		<li>
-			<h3> acompanhe </h3>
+			<h3>acompanhe nas redes sociais</h3>
 		</li>
 
-		<li>
-			<a href="https://br.pinterest.com/aptoxblog/" target="_blank" rel="noopener">
-				<img
-					src="<?php echo esc_url( get_template_directory_uri() . '/assets/icons/ui/ico-pinterest.svg' ); ?>"
-					alt="Pinterest"
-				/>
-				Pinterest
-			</a>
-		</li>
-
-		<li>
-			<a href="https://www.instagram.com/aptox/" target="_blank" rel="noopener">
-				<img
-					src="<?php echo esc_url( get_template_directory_uri() . '/assets/icons/ui/ico-instagram.svg' ); ?>"
-					alt="Instagram"
-				/>
-				Instagram
-			</a>
-		</li>
-
-		<li>
-			<a href="https://www.tiktok.com/@aptoxblog" target="_blank" rel="noopener">
-				<img
-					src="<?php echo esc_url( get_template_directory_uri() . '/assets/icons/ui/ico-tiktok.svg' ); ?>"
-					alt="TikTok"
-				/>
-				TikTok
-			</a>
-		</li>
-
-		<li>
-			<a href="https://www.facebook.com/aptox" target="_blank" rel="noopener">
-				<img
-					src="<?php echo esc_url( get_template_directory_uri() . '/assets/icons/ui/ico-facebook.svg' ); ?>"
-					alt="Facebook"
-				/>
-				Facebook
-			</a>
-		</li>
+		<?php foreach ( $social_links as $social_link ) : ?>
+			<li>
+				<a href="<?php echo esc_url( $social_link['url'] ); ?>" target="_blank" rel="noopener">
+					<img
+						src="<?php echo esc_url( get_template_directory_uri() . '/assets/icons/ui/' . $social_link['icon'] ); ?>"
+						alt="<?php echo esc_attr( $social_link['label'] ); ?>"
+					/>
+					<?php echo esc_html( $social_link['label'] ); ?>
+				</a>
+			</li>
+		<?php endforeach; ?>
 	</ul>
 </section>
 
